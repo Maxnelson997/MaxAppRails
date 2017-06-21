25162a839e86b5879a99fca699008b2c6c233bee
Alpine Devcamp <alpine@devcamp.com>
2017-06-21 14:42:54 +0000

    asdf

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :skills
   resources :portfolios
   resources :blogs
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
