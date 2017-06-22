03ff1c05c885b57af9e023bd9f84d8dfbdf4813e
Alpine Devcamp <alpine@devcamp.com>
2017-06-22 17:46:34 +0000

    customized portfoliosdf

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
+  resources :portfolios, except: [:show]
+  resources :blogs
+  
+  
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
-  resources :portfolios
-  resources :blogs
-  
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
