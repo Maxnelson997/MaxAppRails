325287caa715ee358c5df489a52aab06a03712fe
Alpine Devcamp <alpine@devcamp.com>
2017-06-22 17:20:53 +0000

    destory action implemented

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -44,6 +44,22 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end
   
+  # DELETE /portfolios/1
+  # DELETE /portfolios/1.json
+  def destroy
+    # Perform the lookup
+    @portfolio_item = Portfolio.find(params[:id])
+    
+    # Destroy/delete the record
+    @portfolio_item.destroy
+    
+    # Redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio Item has been destroyed' }
+    end
+  end
+
+  
   
   
   
