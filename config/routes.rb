755419bc1c86680128b53b30d4a324d0add4c038
Alpine Devcamp <alpine@devcamp.com>
2017-06-26 17:16:29 +0000

    assahdude

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,11 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   root to: 'pages#home'
 
+ resources :blogs do 
+   member do 
+     get :toggle_status
+   end
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
