39f71a95e030093cf59894505bfea6170bc4bb48
Alpine Devcamp <alpine@devcamp.com>
2017-06-26 16:09:47 +0000

    integrated friendly routes for blog posts

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,7 +11,6 @@ class PortfoliosController < ApplicationController
   # POST /blogs.json
   def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
-
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
