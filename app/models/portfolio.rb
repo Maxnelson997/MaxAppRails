f04d38be5282578b7cea91aa2af6c1a86c03cf52
Alpine Devcamp <alpine@devcamp.com>
2017-06-29 22:10:19 +0000

    image defualts

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,13 @@ class Portfolio < ApplicationRecord
     end
     
     scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
+    
+    after_initialize :set_defaults
+    
+    def set_defaults 
+        self.main_image ||= "http://via.placeholder.com/30x450"
+        self.thumb_image ||= "http://via.placeholder.com/30x150"
+    end
+    
+    
 end
