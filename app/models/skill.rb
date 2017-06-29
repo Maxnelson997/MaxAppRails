63984cb8bdf46a284f15457e88207dadb24f26b5
Alpine Devcamp <alpine@devcamp.com>
2017-06-29 22:18:30 +0000

    Implemented concern for managing image data

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,8 @@
 class Skill < ApplicationRecord
     validates_presence_of :title, :percent_utilized
+    
+    after_initialize :set_defaults
+    def set_defaults 
+        self.badge ||= "http://via.placeholder.com/250x250"
+    end
 end
