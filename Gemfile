753b433bd323ba13ae63c03dbf76a890962c2609
Alpine Devcamp <alpine@devcamp.com>
2017-07-13 18:50:47 +0000

    implemented authorization engine for portfolio items and blogs

--- Gemfile
+++ Gemfile
@@ -58,4 +58,4 @@ gem 'devise', '~> 4.3'
 gem 'bootstrap', '~> 4.0.0.alpha6'
 
 gem 'devcamp_view_tool'
-gem 'petergate', '~> 1.7', '>= 1.7.5'
+gem 'petergate', '~> 1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,24 @@
 Rails.application.routes.draw do
-  
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  resources :topics, only: [:index, :show]
 
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
-  # devise_for :users
-  resources :portfolios, except: [:show]
-  resources :blogs
+  get 'angular-items', to: 'portfolios#angular'
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
-  get 'guides/book'
   get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tech-news', to: 'pages#tech_news'
 
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
-  get 'angular-items', to: 'portfolios#angular'
-  root to: 'pages#home'
-
- resources :blogs do 
-   member do 
-     
-     get :toggle_status
-   end
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
   end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # mount ActionCable.server => '/cable'
+  
+  root to: 'pages#home'
 end
