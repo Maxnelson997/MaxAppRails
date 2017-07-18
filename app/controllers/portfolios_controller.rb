1ddefedcf95bb281fb1058c64151e2c5f34b2aa8
Alpine Devcamp <maxnelson997@gmail.com>
2017-07-18 19:01:59 +0000

    finished carrierwave installation

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,33 +1,32 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show,:destroy]
-    layout 'portfolio'
-    access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
-    
-    def index
-       @portfolio_items = Portfolio.by_position
-    end
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
+  layout 'portfolio'
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all
   
-    def sort 
-      params[:order].each do |key, value|
-        Portfolio.find(value[:id]).update(position: value[:position])
-      end
-      
-      render nothing: true
-    end
-    
-    def angular
-       @angular_portfolio_items = Portfolio.angular
-    end
-    
-    def new
-      @portfolio_item = Portfolio.new
-      3.times { @portfolio_item.technologies.build }
+  def index
+    @portfolio_items = Portfolio.by_position
+  end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
     end
 
-  # POST /blogs
-  # POST /blogs.json
+    render nothing: true
+  end
+
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+
+  def new
+    @portfolio_item = Portfolio.new
+    # 3.times { @portfolio_item.technologies.build }
+  end
+
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
+
     respond_to do |format|
       if @portfolio_item.save
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
@@ -36,16 +35,11 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
-  # GET /portfolios/1/edit
+
   def edit
   end
-  
-    # PATCH/PUT /portfolios/1
-  # PATCH/PUT /portfolios/1.json
+
   def update
-
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -54,39 +48,32 @@ class PortfoliosController < ApplicationController
       end
     end
   end
-  
-  def show 
 
+  def show
   end
-  
-  # DELETE /portfolios/1
-  # DELETE /portfolios/1.json
-  def destroy
-    # Perform the lookup
 
-    
+  def destroy
     # Destroy/delete the record
     @portfolio_item.destroy
-    
+
     # Redirect
     respond_to do |format|
-      format.html { redirect_to portfolios_url, notice: 'Portfolio Item has been destroyed' }
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
     end
   end
 
-  
-  private 
-  
+  private
+
   def portfolio_params
     params.require(:portfolio).permit(:title,
                                       :subtitle,
-                                      :body, 
+                                      :body,
                                       :main_image,
                                       :thumb_image,
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:name]
+                                     )
   end
-  
-  
+
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
